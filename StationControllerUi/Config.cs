7e60ad44d98c9e91eaeccb53fff916d57705a049
Martin <foeller.martin@googlemail.com>
2020-02-10 14:10:40 +0100

    changed config class

--- StationControllerUi/Config.cs
+++ StationControllerUi/Config.cs
@@ -10,7 +10,6 @@ namespace StationControllerUi
 {
     public class Config
     {
-
         public string ScPath { get; set; }
         public int DebugPort { get; set; }
         public string SyntaxDescriptionFile { get; set; }
